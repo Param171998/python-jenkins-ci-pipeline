7e706b70b8e061588564133cc66fb5574ac3633f
Ubuntu <paramwalia1998@gmail.com>
2024-11-29 12:45:21 +0000

    upated test app py file

--- python-app/test/test_app.py
+++ python-app/test/test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from src.app import hello_world
+from python_app.src.app import hello_world
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World!")
